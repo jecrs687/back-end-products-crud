b6651a12f8a1360be731bbfb012de2255e6f4387
Emanuel Cascone <emanuelcascone@gmail.com>
2023-02-22 22:01:48 -0300

    fix: remove authorization

--- src/api/middlewares/authorization.ts
+++
@@ -1,8 +0,0 @@
-import { type NextFunction, type Request, type Response } from 'express'
-
-export default (req: Request, res: Response, next: NextFunction): Response => {
-     if (!req.headers.authorization.includes('Seedz')) {
-          return res.status(401).end()
-     }
-     next()
-}
